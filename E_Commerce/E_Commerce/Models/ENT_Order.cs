70e11bd26bbc57b42cff221dac3e7b37a7174d03
Muhammed Ashiq <ashiqpkmuhammed@gmail.com>
2023-03-06 10:56:36 +0530

    commit order

---
+++ E_Commerce/E_Commerce/Models/ENT_Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class ENT_Order
+    {
+        public int order_id { get; set; }
+        public int user_id { get; set; }
+        public int product_id { get; set; }
+        public int quantity { get; set; }
+        public int total_price { get; set; }
+        public string status { get; set; }
+        public string createdBy { get; set; }
+        public string createdDate { get; set; }
+        public string lastModifiedBy { get; set; }
+        public string lastModifiedDate { get; set; }
+    }
+}
